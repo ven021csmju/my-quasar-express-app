1306953a583ced7a329f77fe9c50f1f6f2e4f697
Kittiku <ktkwenw@gmail.com>
2025-11-26 02:37:03 +0700

    feat: add Longan Map page with Docker build (Quasar SPA + Nginx)

---
+++ frontend/src/pages/LonganMap.vue
@@ -0,0 +1,15 @@
+<template>
+  <div />
+</template>
+
+<script>
+export default {
+  name: 'LonganMapRedirect',
+  mounted () {
+    // Redirect to the static HTML placed in /public
+    if (typeof window !== 'undefined') {
+      window.location.href = '/longan_map.html'
+    }
+  }
+}
+</script>
